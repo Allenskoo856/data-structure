48a22f27c10cf1c8f94e3f6266ffbef82710b1f0
ZongLUnli <mrzonglunli@gmail.com>
2018-11-23 14:41:07 +0800

    commit dataStruct.Map

---
+++ src/dataStruct/map/BSTMap.java
@@ -0,0 +1,44 @@
+package dataStruct.map;
+
+/**
+ * @author : Administrator
+ * @create 2018-11-23 14:39
+ */
+public class BSTMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+
+    @Override
+    public void add(K key, V value) {
+
+    }
+
+    @Override
+    public V remove(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public void set(K key, V newValue) {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
