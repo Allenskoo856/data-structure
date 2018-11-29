6b2bd91e33af76ad85f715b04e502ffa233a9030
ZongLUnli <mrzonglunli@gmail.com>
2018-11-29 20:37:06 +0800

    commit hashTable.java

---
+++ src/dataStruct/hash/HashTable.java
@@ -0,0 +1,71 @@
+package dataStruct.hash;
+
+import java.util.TreeMap;
+
+/**
+ * @author : Administrator
+ * @create 2018-11-27 18:45
+ */
+public class HashTable<K, V> {
+
+    private TreeMap<K, V>[] hashTable;
+    private int M;
+    private int size;
+
+    public HashTable(int M) {
+        this.M = M;
+        size = 0;
+        hashTable = new TreeMap[M];
+        for (int i = 0; i < M; i++) {
+            hashTable[i] = new TreeMap<>();
+        }
+    }
+
+    public HashTable() {
+        this(97);
+    }
+
+    private int hash(K key) {
+        return (key.hashCode() & 0x7ffffff) % M;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(K key, V value) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if (map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            map.put(key, value);
+            size++;
+        }
+    }
+
+    public V remove(K key) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        V ret = null;
+        if (map.containsKey(key)) {
+            ret = map.remove(key);
+            size--;
+        }
+        return ret;
+    }
+
+    public void set(K key, V value) {
+        TreeMap<K, V> map = hashTable[hash(key)];
+        if (!map.containsKey(key)) {
+            throw new IllegalArgumentException(key + "doesn't exits!");
+        }
+    }
+
+    public boolean contains(K key) {
+        return hashTable[hash(key)].containsKey(key);
+    }
+
+    public V get(K key) {
+        return hashTable[hash(key)].get(key);
+    }
+
+}
