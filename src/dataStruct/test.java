7f6eefbb8d62d8f97ccc041f9ee6c9827855e97f
ZongLUnli <mrzonglunli@gmail.com>
2018-12-05 16:41:14 +0800

    commit doc

---
+++ src/dataStruct/test.java
@@ -0,0 +1,13 @@
+package dataStruct;
+
+import java.util.Collection;
+
+/**
+ * @author : Administrator
+ * @create 2018-11-29 21:23
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
